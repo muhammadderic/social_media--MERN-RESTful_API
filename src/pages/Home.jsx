465509a9ae905e0c76d1ad2e5af99b8347d87c78
muhammadderic <muhammad.donny.e@gmail.com>
2023-04-30 12:53:41 +0700

    Create front end - home - Sidebar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import Topbar from "../components/Topbar";
+// import Topbar from "../components/Topbar";
+import Sidebar from "../components/Sidebar";
 import "../styles/home.page.css";
 
 function Home() {
   return (
     <>
-      <Topbar />
-      <div className="homeContainer"></div>
+      {/* <Topbar /> */}
+      <div className="homeContainer">
+        <Sidebar />
+      </div>
     </>
   )
 }
