0b53b6732a227aea428eaf9e021e2ecfd00d47ed
muhammadderic <muhammad.donny.e@gmail.com>
2023-04-30 13:06:39 +0700

    Add dummyData.js for users and posts

---
+++ src/dummyData.js
@@ -0,0 +1,99 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: "http://placekitten.com/32/32",
+    username: "Persiano Cat Go",
+  },
+  {
+    id: 2,
+    profilePicture: "http://placekitten.com/33/33",
+    username: "Mainener Coon Highcat",
+  },
+  {
+    id: 3,
+    profilePicture: "http://placekitten.com/34/34",
+    username: "Siamesesian Cattie",
+  },
+  {
+    id: 4,
+    profilePicture: "http://placekitten.com/35/35",
+    username: "Bengalenda",
+  },
+  {
+    id: 5,
+    profilePicture: "http://placekitten.com/36/36",
+    username: "Birman ju Ge",
+  },
+  {
+    id: 6,
+    profilePicture: "http://placekitten.com/37/37",
+    username: "Chinchillay Sempro",
+  },
+  {
+    id: 7,
+    profilePicture: "http://placekitten.com/38/38",
+    username: "Scot Tish Oldf",
+  },
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "Love For All, Hatred For None.",
+    photo: "https://picsum.photos/200",
+    date: "5 mins ago",
+    userId: 1,
+    like: 32,
+    comment: 9,
+  },
+  {
+    id: 2,
+    photo: "https://picsum.photos/201",
+    date: "15 mins ago",
+    userId: 2,
+    like: 2,
+    comment: 1,
+  },
+  {
+    id: 3,
+    desc: "Every moment is a fresh beginning.",
+    photo: "https://picsum.photos/202",
+    date: "1 hour ago",
+    userId: 3,
+    like: 61,
+    comment: 2,
+  },
+  {
+    id: 4,
+    photo: "https://picsum.photos/203",
+    date: "4 hours ago",
+    userId: 4,
+    like: 7,
+    comment: 3,
+  },
+  {
+    id: 5,
+    photo: "https://picsum.photos/204",
+    date: "5 hours ago",
+    userId: 5,
+    like: 23,
+    comment: 5,
+  },
+  {
+    id: 6,
+    photo: "https://picsum.photos/205",
+    date: "1 day ago",
+    userId: 6,
+    like: 44,
+    comment: 6,
+  },
+  {
+    id: 7,
+    desc: "Never regret anything that made you smile.",
+    photo: "ahttps://picsum.photos/206",
+    date: "2 days ago",
+    userId: 7,
+    like: 52,
+    comment: 3,
+  },
+];
